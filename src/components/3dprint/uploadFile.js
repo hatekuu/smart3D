f2f725425c86730905a6cd394287fbcff1bbb624
unknown <hatekuu@gmail.com>
2025-02-12 09:46:03 +0700

    dong1231

--- src/components/3dprint/uploadFile.js
+++ src/components/3dprint/uploadFile.js
@@ -61,7 +61,7 @@ const UploadFile = ({ user, printId }) => {
         const chunkSize = 2 * 1024 * 1024; // 2MB mỗi chunk
         const totalChunks = Math.ceil(file.size / chunkSize);
         const uploadUrl = `${process.env.REACT_APP_API_URL}/3dprint/upload`;
-
+        
         const fileId = generateFileId();
         const quantity = quantities[file.name] || 1;
         let start = 0;
