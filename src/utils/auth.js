8e9642fd0165d1fabbb86cfab3f9ac60a561840c
unknown <hatekuu@gmail.com>
2025-02-13 20:43:21 +0700

    dong

--- src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-export const getRoleFromToken = () => {
-  const token = localStorage.getItem('token');
-  if (!token) return null;
-
-  // Decode token to get role (assuming JWT)
-  const payload = JSON.parse(atob(token.split('.')[1]));
-  return payload.role;
-};
--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'http://localhost:5000/api';
-export const ROLES = {
-  USER: 'user',
-  MANAGER: 'manager',
-};
